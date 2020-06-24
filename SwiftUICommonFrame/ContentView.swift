33d3f42b6c83b7663a58d673e827a538986e3d29
levdnaga <levdnaga@mediba.jp>
2020-06-25 05:23:19 +0900

    Initial Commit

---
+++ SwiftUICommonFrame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICommonFrame
+//
+//  Created by 永田大祐 on 2020/06/25.
+//  Copyright © 2020 永田大祐. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
