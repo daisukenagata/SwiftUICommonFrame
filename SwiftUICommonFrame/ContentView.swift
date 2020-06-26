c708a740499aff072311b7b266283c25ae83ffdb
levdnaga <levdnaga@mediba.jp>
2020-06-26 22:02:36 +0900

    add

--- SwiftUICommonFrame/ContentView.swift
+++ SwiftUICommonFrame/ContentView.swift
@@ -11,22 +11,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            SetDesgin(c: Color.red) {Text("name") }
+            SetDesgin(c: Color.red) { Text("name") }
             SetDesgin(c: Color.yellow) {Text("name") }
             SetDesgin(c: Color.green) {Text("name") }
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
 private struct SetDesgin<Content:View>: View {
     
-    var c: Color
+    let c: Color
     let viewBuilder: () -> Content
     var body: some View {
         viewBuilder()
